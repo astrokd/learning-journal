5ff667de8d850c097171629153474b45681dcb62
AstroKD <kdreyerinseattle@gmail.com>
2019-09-30 15:59:14 -0700

    class demo js file

---
+++ Class.js
@@ -0,0 +1,12 @@
+
+function forTest() {
+    var result = 'start';
+
+    for (var i = 0; i < 5; i++) {
+        result = result + ' ' + i;
+        console.log('inside the for loop',result);
+    }
+    console.log('after the for loop', result);
+}
+
+varible a equals Welcome;
